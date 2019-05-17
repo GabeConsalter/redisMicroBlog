c5f57195b1226ebb5d0f47fefee8db341b0bde61
Gabriel Consalter <consaltergabriel@gmail.com>
2019-05-17 20:49:01 -0300

    Regis config

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
+const redis = require('redis');
+const client = redis.createClient();
 
 app.get('/', (req, res) => {
 	res.send('Hello World');
 });
+
+client.on('connect', () => console.log('Redis client connected'));
+client.on('error', err => console.error('Redis error:', err));
 
 app.listen(3000, () => console.log('Running on 3000'));
